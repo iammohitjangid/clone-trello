499e4010306fa3b70d5eb938150f7316d11f7201
mohit jangid <mohitsharmaj304@gmail.com>
2021-03-18 14:59:35 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React,{useState}from 'react';
 import Wrapper from "./components/Wrapper";
 import Navigation from "./components/Nav/Navigation";
 import { makeStyles } from '@material-ui/core';
 
 const useStyle = makeStyles((theme)=>({
   root:{
-    background:"green"
   }
 }))
 export default function App() {
+  const [backgroundImage,setBackgroundImage] = useState("green"); 
   const classes = useStyle();
   return (
-    <div className={classes.root}>
-      <Navigation />
+    <div className={classes.root}
+    style={{
+      background:backgroundImage,
+      backgroundImage:`url("${backgroundImage}")`,
+      backgroundRepeat:"no-repeat",
+      backgroundSize:"cover",
+       
+   }}>
+      <Navigation setBgImg ={setBackgroundImage} />
       <Wrapper/>
     </div>
   )
--- src/components/Nav/TopBar.js
+++ src/components/Nav/TopBar.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 const useStyle = makeStyles((theme) => ({
     AppBar: {
-      background:"green"
+      background:"transparent"
     },
     title:{
         flexGrow:1
